6590f03448a137ff1877dec638068bad14040c9b
charliel <charliel@ca.ibm.com>
2019-11-20 14:26:30 -0500

    Update index.md

--- config.rb
+++ config.rb
@@ -15,10 +15,10 @@ set :markdown,
     renderer: UniqueHeadCounter
 
 # Assets
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :fonts_dir, 'fonts'
+# set :css_dir, 'stylesheets'
+# set :js_dir, 'javascripts'
+# set :images_dir, 'images'
+# set :fonts_dir, 'fonts'
 
 # Activate the syntax highlighter
 activate :syntax

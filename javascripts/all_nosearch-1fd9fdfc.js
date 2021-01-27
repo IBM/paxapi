de16beb18345368055a9223cd3de66319b655e05
charliel <charliel@ca.ibm.com>
2021-01-26 21:35:41 -0500

    publish: Merge pull request #2 from IBM/imgbot
    generated from commit 13305916091abe6c6c0699848a50971fb01d6c14

---
+++ javascripts/all_nosearch-1fd9fdfc.js
@@ -0,0 +1,1 @@
+$(function(){loadToc($("#toc"),".toc-link",".toc-list-h2",10),setupLanguages($("body").data("languages")),$(".content").imagesLoaded(function(){window.recacheHeights(),window.refreshToc()})}),window.onpopstate=function(){activateLanguage(getLanguageFromQueryString())};
